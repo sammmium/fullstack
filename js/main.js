94392683786872e95b977e9de67bff15a6d78961
sammmium <sammmium.dev@gmail.com>
2023-01-29 19:21:32 +0300

    начало обучения

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('a.menu-home').on('click', function() {
+        alert('The page says The page says The page says The page says The page says The page says ');
+        console.log('sdsdgsdgsdhsdhsdh');
+        $('.content').html('главная страница сверху');
+    });
+});
